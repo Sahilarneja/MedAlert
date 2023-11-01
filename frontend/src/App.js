7f02671b54f0201f287a7de7ca74702ae37632d6
sanyam <sanyamnarang40@gmail.com>
2023-11-01 13:36:59 +0530

    code updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginSignup from './Components/LoginSignup';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DisplayData from './Components/DisplayData';
+import UserPortal from './Components/UserPortal';
+import UpdateData from './Components/UpdateData';
+import AddData from './Components/AddData';
+import Notfound from './Components/Notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<LoginSignup pageTitle="Billing-Login/SignUp" />} />
+        <Route path="/Admin-Portal" element={<DisplayData pageTitle="Admin Portal" />} />
+        <Route path="/User-Portal" element={<UserPortal pageTitle="User Portal" />} />
+        <Route path="/Admin/UpdateData" element={<UpdateData pageTitle="Admin-Update" />} />
+        <Route path="/Admin/AddData" element={<AddData pageTitle="Admin-ADD" />} />
+        <Route path="*" element={<Notfound pageTitle="404 - Not Found" />} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
